4c057f8a7efcaf3605d5be7185c6035c0c6d24f4
snehakweera77 <snehakweera77@gmail.com>
2021-08-10 10:20:10 +0530

    Increasing Subsequence

---
+++ dp/src/com/company/IncreasingSubsequence.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class IncreasingSubsequence {
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+        FastReader fsr = new FastReader();
+        int n = fsr.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++)
+            nums[i] = fsr.nextInt();
+        int[] dp = new int[nums.length];
+        int len = 0;
+
+        for(int x : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, x);
+            if(i < 0) i = -(i + 1);
+            dp[i] = x;
+            if(i == len) len++;
+        }
+        System.out.println(len);
+    }
+
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
